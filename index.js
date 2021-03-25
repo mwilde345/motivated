b7351761455e2ee9c26b13d984148e43aeaa9af3
Matt Wilde <wildmat@amazon.com>
2021-03-24 21:07:39 -0700

    added dynamo table to serverless config and started building queries

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 "use strict";
 const bent = require("bent");
 const formurlencoded = require("form-urlencoded").default;
+const twilio = require("twilio");
+const AWS = require("aws-sdk");
+
+var accountSid = process.env.TWILIO_ACC_ID; // Your Account SID from www.twilio.com/console
+var authToken = process.env.TWILIO_AUTH; // Your Auth Token from www.twilio.com/console
+const twilioClient = new twilio(accountSid, authToken);
+const dynamo = new AWS.DynamoDB();
 
 module.exports.handler = async (event) => {
   const getJSON = bent("json");
@@ -30,17 +37,43 @@ module.exports.handler = async (event) => {
     Authorization: `Bearer ${auth_token}`,
     "User-Agent": "motivated:1.0 by miraclebob",
   });
-  let subParams = formurlencoded({ limit: 10, t: "day" });
+  let subParams = formurlencoded({ limit: 2, t: "day" });
   const response = await get("r/GetMotivated/hot?" + subParams);
   let { after, children } = response.data;
   console.log(children.length);
   let validPosts = children.filter((o) => {
     return o.data.post_hint === "image" && o.data.is_self === false;
   });
-  await validPosts.forEach(async (o) => {
-    console.log(o.data.url)
-    console.log((await getBuffer(o.data.url)));
-  });
+
+  let params = {
+    ExpressionAttributeNames: {
+      "#AT": "AlbumTitle",
+      "#ST": "SongTitle",
+    },
+    ExpressionAttributeValues: {
+      ":a": {
+        S: "No One You Know",
+      },
+    },
+    FilterExpression: "Artist = :a",
+    ProjectionExpression: "#ST, #AT",
+    TableName: "Music",
+  };
+
+  let alreadySentFiles = dynamo.scan({});
+
+  for (const post of validPosts) {
+    console.log(post.data.url);
+    console.log(await getBuffer(post.data.url));
+
+    await twilioClient.messages
+      .create({
+        to: "+14355925998", // Text this number
+        from: "+12067370668", // From a valid Twilio number
+        mediaUrl: post.data.url,
+      })
+      .then((message) => console.log(message.sid));
+  }
 
   // let buffer = await getBuffer("http://site.com/image.png");
   // let obj = await getJSON("http://site.com/json.api");
