ec8fe1b94d42ae3d475c5013e57b11b6b87878f7
Matt Wilde <wildmat@amazon.com>
2021-03-25 16:45:21 -0700

    checks dynamo for already sent files

--- index.js
+++ index.js
@@ -10,86 +10,139 @@ const twilioClient = new twilio(accountSid, authToken);
 const dynamo = new AWS.DynamoDB();
 
 module.exports.handler = async (event) => {
-  const getJSON = bent("json");
-  const getBuffer = bent("buffer");
+    const getJSON = bent("json");
+    const getBuffer = bent("buffer");
 
-  const post = bent(
-    200,
-    "POST",
-    "json",
-    {
-      Authorization: `Basic ${Buffer.from(
-        `${process.env.REDDIT_ID}:${process.env.REDDIT_SECRET}`
-      ).toString("base64")}`,
-      "User-Agent": "motivated:1.0 by miraclebob",
-    },
-    "https://www.reddit.com/api/v1/"
-  );
-  let params = formurlencoded({
-    grant_type: "password",
-    username: process.env.REDDIT_UNAME,
-    password: process.env.REDDIT_PWD,
-  });
-  const auth_token = (await post("access_token?" + params)).access_token;
-  console.log(auth_token);
+    const post = bent(
+        200,
+        "POST",
+        "json",
+        {
+            Authorization: `Basic ${Buffer.from(
+                `${process.env.REDDIT_ID}:${process.env.REDDIT_SECRET}`
+            ).toString("base64")}`,
+            "User-Agent": "motivated:1.0 by miraclebob",
+        },
+        "https://www.reddit.com/api/v1/"
+    );
+    let params = formurlencoded({
+        grant_type: "password",
+        username: process.env.REDDIT_UNAME,
+        password: process.env.REDDIT_PWD,
+    });
+    const auth_token = (await post("access_token?" + params)).access_token;
+    console.log(auth_token);
 
-  const get = bent("https://oauth.reddit.com/", "GET", "json", 200, {
-    Authorization: `Bearer ${auth_token}`,
-    "User-Agent": "motivated:1.0 by miraclebob",
-  });
-  let subParams = formurlencoded({ limit: 2, t: "day" });
-  const response = await get("r/GetMotivated/hot?" + subParams);
-  let { after, children } = response.data;
-  console.log(children.length);
-  let validPosts = children.filter((o) => {
-    return o.data.post_hint === "image" && o.data.is_self === false;
-  });
+    const get = bent("https://oauth.reddit.com/", "GET", "json", 200, {
+        Authorization: `Bearer ${auth_token}`,
+        "User-Agent": "motivated:1.0 by miraclebob",
+    });
+    let subParams = formurlencoded({ limit: 3, t: "day" });
+    const response = await get("r/GetMotivated/hot?" + subParams);
+    let { after, children } = response.data;
+    console.log(children.length);
+    let validPostUrls = children
+        .filter((o) => {
+            return o.data.post_hint === "image" && o.data.is_self === false;
+        })
+        .map((o) => o.data.url);
+    console.log(validPostUrls);
+	console.log(validPostUrls.map((url) => ({
+		'file_url': { S: url },
+	})))
+    let getItemsParams = {
+        RequestItems: {
+            'MotivationDDBTable': {
+                ExpressionAttributeNames: {
+                    "#F": "file_url",
+                    "#T": "time_sent",
+                },
+                Keys: validPostUrls.map((url) => ({
+                    'file_url': { 'S': url },
+                })),
+                ProjectionExpression: "#F, #T",
+            },
+        },
+    };
 
-  let params = {
-    ExpressionAttributeNames: {
-      "#AT": "AlbumTitle",
-      "#ST": "SongTitle",
-    },
-    ExpressionAttributeValues: {
-      ":a": {
-        S: "No One You Know",
-      },
-    },
-    FilterExpression: "Artist = :a",
-    ProjectionExpression: "#ST, #AT",
-    TableName: "Music",
-  };
+	// var params = {
+	// 	RequestItems: {
+	// 	  'TABLE_NAME': {
+	// 		Keys: [
+	// 		  {'KEY_NAME': {N: 'KEY_VALUE_1'}},
+	// 		  {'KEY_NAME': {N: 'KEY_VALUE_2'}},
+	// 		  {'KEY_NAME': {N: 'KEY_VALUE_3'}}
+	// 		],
+	// 		ProjectionExpression: 'KEY_NAME, ATTRIBUTE'
+	// 	  }
+	// 	}
+	//   };
 
-  let alreadySentFiles = dynamo.scan({});
+    let alreadySentFiles = await new Promise((res, rej) => {
+        dynamo.batchGetItem(getItemsParams, (err, data) => {
+            if (err) {
+                console.log(err);
+                rej(err);
+            } else {
+				res(data.Responses.MotivationDDBTable);
+			}
+        });
+    });
+    console.log('sent already: ', alreadySentFiles);
+	const leftover = validPostUrls.filter(i => {
+		return !(alreadySentFiles.map(o => o.file_url.S).includes(i))
+	});
+	console.log('leftover', leftover);
+    for (const url of leftover) {
+        await twilioClient.messages
+            .create({
+                to: "+14355925998", // Text this number
+                from: "+12067370668", // From a valid Twilio number
+                mediaUrl: url,
+            })
+            .then((message) => console.log(message.sid));
+        await new Promise((res, rej) => {
+            dynamo.putItem(
+                {
+                    Item: {
+                        file_url: {
+                            S: url,
+                        },
+                        time_sent: {
+                            S: new Date().toString(),
+                        },
+                    },
+                    TableName: "MotivationDDBTable",
+                },
+                (err, data) => {
+                    if (err) {
+                        console.log(err);
+                        rej(err);
+                    } else {
+						console.log(`saved ${url} to db`);
+						res(data);
+					}
+                }
+            );
+        });
+    }
 
-  for (const post of validPosts) {
-    console.log(post.data.url);
-    console.log(await getBuffer(post.data.url));
+    // let buffer = await getBuffer("http://site.com/image.png");
+    // let obj = await getJSON("http://site.com/json.api");
 
-    await twilioClient.messages
-      .create({
-        to: "+14355925998", // Text this number
-        from: "+12067370668", // From a valid Twilio number
-        mediaUrl: post.data.url,
-      })
-      .then((message) => console.log(message.sid));
-  }
+    return {
+        statusCode: 200,
+        body: JSON.stringify(
+            {
+                message:
+                    "Go Serverless v1.0! Your function executed successfully!",
+                input: event,
+            },
+            null,
+            2
+        ),
+    };
 
-  // let buffer = await getBuffer("http://site.com/image.png");
-  // let obj = await getJSON("http://site.com/json.api");
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v1.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
